64208a1eeb517afcfa69c0434576deefdae9c091
Pedro Franceschi <pedrohfranceschi@gmail.com>
2013-04-07 02:32:34 -0300

    Don't escape parameters when encrypting card_hash

--- lib/pagarme/utils.rb
+++ lib/pagarme/utils.rb
@@ -1,7 +1,5 @@
-require 'uri'
-
 class Hash
   def to_params
-	URI.escape(self.collect{ |key, value| "#{key}=#{value}" }.join('&'))
+	self.collect{ |key, value| "#{key}=#{value}" }.join('&')
   end
 end
