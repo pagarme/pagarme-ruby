2ec3bfcee6de195a0f5a66713146edcca7ad3509
Pedro Franceschi <pedrohfranceschi@gmail.com>
2013-04-07 01:27:46 -0300

    Fix identation in test code

--- pagarme.rb
+++ pagarme.rb
@@ -4,26 +4,24 @@ PagarMe.api_key = "Jy1V5bJcGf8q4gHepttt"
 PagarMe.live = true
 
 begin
+  transaction = PagarMe::Transaction.new
 
-transaction = PagarMe::Transaction.new
+  transaction.card_number = "0000000000000000"
+  transaction.card_holder_name = "Test User"
+  transaction.card_expiracy_month = "12"
+  transaction.card_expiracy_year = "15"
+  transaction.card_cvv = "314"
 
-transaction.card_number = "0000000000000000"
-transaction.card_holder_name = "Test User"
-transaction.card_expiracy_month = "12"
-transaction.card_expiracy_year = "15"
-transaction.card_cvv = "314"
+  transaction.charge
 
-transaction.charge
+  transaction.chargeback
 
-transaction.chargeback
+  chargebacked_transaction = PagarMe::Transaction.find_by_id(transaction.id)
 
-chargebacked_transaction = PagarMe::Transaction.find_by_id(transaction.id)
+  puts chargebacked_transaction.id == transaction.id
+  puts chargebacked_transaction.status == transaction.status
 
-puts chargebacked_transaction.id == transaction.id
-puts chargebacked_transaction.status == transaction.status
-
-puts chargebacked_transaction.inspect
-
+  puts chargebacked_transaction.inspect
 rescue PagarMe::PagarMeError => e
   puts "Error: #{e}"
 end
