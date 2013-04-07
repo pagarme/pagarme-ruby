313fd3bf292c3ff15597c2437c3253d5716ec044
Pedro Franceschi <pedrohfranceschi@gmail.com>
2013-04-07 02:14:37 -0300

    Add test code for transaction initialized with card_hash

--- pagarme.rb
+++ pagarme.rb
@@ -11,6 +11,7 @@ begin
   transaction.card_expiracy_month = "12"
   transaction.card_expiracy_year = "15"
   transaction.card_cvv = "314"
+  transaction.amount = 1000
 
   transaction.charge
 
@@ -22,6 +23,13 @@ begin
   puts chargebacked_transaction.status == transaction.status
 
   puts chargebacked_transaction.inspect
+
+  hash_transaction = PagarMe::Transaction.new("bQQfLVIs0UB5qR28Cx3flweO5aIm9clBVJdlbgBHmKC418YVvrZR4tDBsuERitydeDqMqk6dXYWTHit4u2kHTOepqyXaKTuJVasYL6IiC0lEDhkRxcqbJeOKAAjsru4vTTP3V0XftAVwZ9ehFoGFTEZCnDFus3SSvZrY27HjOJ+VepWDzP4yq46A8n8RR9h6WhD3CmqYNSGOCvJGA5hO0j3CFQGFqcX1gQP7QUQwncEmZYVYjXG7B2W+jFTovjm/1DicmPcdHXxfHbtt7osgo/d4JYmM44BeNg6FzqSzphOuM+zceCu+Pn1QrRXJxUPi2DRSXnvz2EQjqdTyENK/hA==")
+  hash_transaction.amount = 1000
+
+  hash_transaction.charge
+
+  puts hash_transaction.inspect
 rescue PagarMe::PagarMeError => e
   puts "Error: #{e}"
 end
