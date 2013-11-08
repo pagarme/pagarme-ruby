16c77bb2ac8cae34b5e77c1cd84b572de60334b1
Henrique Dubugras <hdubugras@gmail.com>
2013-11-08 14:51:43 -0200

    Add missing subscription test file

---
+++ test/pagarme/subscription.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require File.expand_path('../../test_helper', __FILE__)
+module PagarMe
+	class SubscriptionTest < Test::Unit::TestCase
+	  should 'be able to create subscription with plan' do
+		plan = test_plan
+		plan.create
+		subscription = test_subscription
+		subscription.plan = plan
+		subscription.create
+		test_plan_response(subscription.plan)
+		test_transaction_with_customer(subscription)
+	  end
+
+	  should 'be able to create subscription without plan' do
+		subscription = test_subscription({:amount => 2000})
+		subscription.create
+		assert subscription.transactions.length == 1
+		subscription.charge(2000)
+		assert subscription.transactions.length == 2
+		assert subscription.transactions.first.kind_of?(PagarMe::Transaction)
+		subscription.transactions.each do |t|
+		  test_subscription_transaction_response(t)
+		end
+	  end
+		
+	end
+end
