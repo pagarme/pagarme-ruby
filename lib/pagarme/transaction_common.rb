6c41c51d3b25df0860c229588ef646da324e4b6f
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2014-10-28 16:04:45 -0200

    Remove repeated code.

--- lib/pagarme/transaction_common.rb
+++ lib/pagarme/transaction_common.rb
@@ -4,44 +4,38 @@ require File.join(File.dirname(__FILE__), '..', 'pagarme')
 module PagarMe
   class TransactionCommon < Model
 
-    def initialize(response = {})
-      super(response)
-      self.payment_method = 'credit_card' unless self.payment_method
-      self.installments = 1  unless self.installments
-      self.status = 'local' unless self.status
-    end
+	def initialize(response = {})
+	  super(response)
+	  self.payment_method = 'credit_card' unless self.payment_method
+	  self.installments = 1  unless self.installments
+	  self.status = 'local' unless self.status
+	end
 
-    def create
-      self.card_hash = generate_card_hash unless self.card_hash
-      self.card_number = nil
-      self.card_holder_name = nil
-      self.card_expiration_year = nil
-      self.card_expiration_month = nil
-      self.card_cvv = nil
-      super
-    end
+	def create
+	  self.card_hash = generate_card_hash unless self.card_hash
+	  self.card_number = nil
+	  self.card_holder_name = nil
+	  self.card_expiration_year = nil
+	  self.card_expiration_month = nil
+	  self.card_cvv = nil
+	  super
+	end
 
-    def card_data_parameters
-      {
-        :card_number => self.card_number,
-        :card_holder_name => self.card_holder_name,
-        :card_expiration_date => "#{self.card_expiration_month}#{self.card_expiration_year}",
-        :card_cvv => self.card_cvv
-      }
-    end
+	def card_data_parameters
+	  {
+		:card_number => self.card_number,
+		:card_holder_name => self.card_holder_name,
+		:card_expiration_date => "#{self.card_expiration_month}#{self.card_expiration_year}",
+		:card_cvv => self.card_cvv
+	  }
+	end
 
-    def generate_card_hash
-      request = PagarMe::Request.new("/transactions/card_hash_key", 'GET')
-      response = request.run
-	  card_data = {
-        :card_number => self.card_number,
-        :card_holder_name => self.card_holder_name,
-        :card_expiration_date => "#{self.card_expiration_month}#{self.card_expiration_year}",
-        :card_cvv => self.card_cvv
-      }
+	def generate_card_hash
+	  request = PagarMe::Request.new("/transactions/card_hash_key", 'GET')
+	  response = request.run
 
-      public_key = OpenSSL::PKey::RSA.new(response['public_key'])
-      ret = "#{response['id']}_#{Base64.strict_encode64(public_key.public_encrypt(card_data.to_params))}"
-    end
+	  public_key = OpenSSL::PKey::RSA.new(response['public_key'])
+	  ret = "#{response['id']}_#{Base64.strict_encode64(public_key.public_encrypt(card_data_parameters.to_params))}"
+	end
   end
 end
