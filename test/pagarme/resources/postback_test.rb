18e8d0b9ecd487087f458c40d5516e59698a7873
Dalton <dalton.pinto@pagar.me>
2016-03-18 20:09:19 -0300

    Validates postback with digest kind

--- test/pagarme/resources/postback_test.rb
+++ test/pagarme/resources/postback_test.rb
@@ -10,14 +10,14 @@ module PagarMe
     end
 
     should 'be valid when has invalid signature' do
-      postback = PagarMe::Postback.new postback_response_params(signature: 'invalid signature')
+      postback = PagarMe::Postback.new postback_response_params(signature: 'sha1=invalid signature')
       assert !postback.valid?
     end
 
     should 'validate signature' do
       params = postback_response_params
-      assert  PagarMe::Postback.valid_request_signature?(params[:payload], "sha1=#{params[:signature]}")
-      assert !PagarMe::Postback.valid_request_signature?(params[:payload], params[:signature])
+      assert  PagarMe::Postback.valid_request_signature?(params[:payload], params[:signature])
+      assert !PagarMe::Postback.valid_request_signature?(params[:payload], params[:signature][4..-1])
       assert !PagarMe::Postback.valid_request_signature?(params[:payload], 'invalid signature')
     end
   end
