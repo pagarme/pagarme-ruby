0c6e0bdc34f73b5471af061a9d51504c26617c28
Alexandre de Souza <xadee.13@gmail.com>
2016-07-07 19:10:21 -0300

    Updated generate_card_hash and updated fix tests

--- lib/pagarme/resources/transaction.rb
+++ lib/pagarme/resources/transaction.rb
@@ -37,6 +37,11 @@ module PagarMe
       
     def generate_card_hash(encryption_key)
       raise RequestError.new('Invalid Encryption Key') unless encryption_key
+      
+      if encryption_key == ''
+        raise RequestError.new('Invalid Encryption Key')
+      end
+
       PagarMe::Request.get(url('card_hash_key'), params: { encryption_key: encryption_key }).call
     end
       alias :charge :create
